84e3fce19a945908870c9305ba18bbf018c92c9f
Tarun_Dutta <008446289@coyote.csusb.edu>
2024-04-06 21:11:06 -0700

    Revert "dog counter"
    This reverts commit 258e3a08f0b1cf518c7ca218dd6dc6bd941e7ff7.

--- Assets/Pausemenu.cs
+++ Assets/Pausemenu.cs
@@ -1,43 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class Pausemenu : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenuPanel;
+    [SerializeField] public GameObject pausemenuPanel;
 
     public void Pause()
     {
+        pausemenuPanel.SetActive(true);
         Time.timeScale = 0f;
-        SetPauseMenuActive(true);
     }
 
     public void Resume()
     {
+        pausemenuPanel.SetActive(false);
         Time.timeScale = 1f;
-        SetPauseMenuActive(false);
     }
 
     public void Restart()
     {
+        pausemenuPanel.SetActive(false);
         Time.timeScale = 1f;
         SceneManager.LoadScene("subtraction");
     }
 
     public void Exit()
     {
+        pausemenuPanel.SetActive(false);
         Time.timeScale = 1f;
         SceneManager.LoadScene("UI");
     }
 
-    private void SetPauseMenuActive(bool active)
-    {
-        if (pauseMenuPanel != null)
-        {
-            pauseMenuPanel.SetActive(active);
-        }
-        else
-        {
-            Debug.LogError("Pause menu panel is not assigned!");
-        }
-    }
 }
--- Assets/subtraction_code.cs
+++ Assets/subtraction_code.cs
@@ -1,17 +1,13 @@
 using TMPro;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System;
-using System.Collections;
-using System.IO;
 using UnityEngine.SceneManagement;
 
 public class SubtractionQuiz : MonoBehaviour
 {
     public TextMeshProUGUI questionText;
     public Button[] answerButtons;
-    public Image[] dogUnits; // Array to represent dog units
     public TextMeshProUGUI feedbackText;
     public TextMeshProUGUI questionNumberText;
     public TextMeshProUGUI scoreText;
@@ -19,19 +15,20 @@ public class SubtractionQuiz : MonoBehaviour
     private int questionAnswer;
     public int totalQuestions;
     private int remainingQuestions;
-    private int currentQuestionNumber = 0;
-    public int correctAnswersCount = 0;
+    private int currentQuestionNumber;
+    private int correctAnswersCount;
 
     private int numberOfChoices = 3;
-    public float accuracy;
-    public float rate;
+    private float accuracy;
+    private float rate;
     private int totalWrongAnswers;
-    public float startTime, endTime, totalTime;
+    private float startTime, endTime, totalTime;
 
     void Start()
     {
         totalQuestions = 5;
-        remainingQuestions = totalQuestions;
+        currentQuestionNumber = 0;
+        remainingQuestions = totalQuestions - currentQuestionNumber;
         UpdateQuestionCounter();
         startTime = Time.time;
         GenerateQuestion();
@@ -48,8 +45,8 @@ public class SubtractionQuiz : MonoBehaviour
             return;
         }
 
-        int operand1 = UnityEngine.Random.Range(1, 10);
-        int operand2 = UnityEngine.Random.Range(1, operand1);
+        int operand1 = Random.Range(1, 10);
+        int operand2 = Random.Range(1, operand1);
         questionAnswer = operand1 - operand2;
 
         currentQuestionNumber++;
@@ -57,50 +54,42 @@ public class SubtractionQuiz : MonoBehaviour
 
         questionText.text = $"{operand1} - {operand2} = __";
 
-        Debug.Log("Before displayDogsForQuestion");
-        Debug.Log("Operand1 - " + operand1);
-        Debug.Log("Operand2 - " + operand2);
-        DisplayDogsForQuestion(operand1, operand2); // Display dogs based on the answer
-
         int[] answerOptions = GenerateAnswerOptions();
-
-        ShuffleArray(answerOptions);
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
             answerButtons[i].GetComponentInChildren<Text>().text = answerOptions[i].ToString();
-            int chosenAnswer = answerOptions[i];
             answerButtons[i].onClick.RemoveAllListeners();
+            int chosenAnswer = answerOptions[i];
             answerButtons[i].onClick.AddListener(() => StartCoroutine(CheckAnswerWithDelay(chosenAnswer)));
         }
     }
 
-    void DisplayDogsForQuestion(int operand1, int operand2)
+    int[] GenerateAnswerOptions()
     {
-        // Deactivate all dog units initially
-        foreach (Image dogUnit in dogUnits)
+        int[] answerOptions = new int[numberOfChoices];
+        int correctAnswerIndex = Random.Range(0, numberOfChoices);
+        answerOptions[correctAnswerIndex] = questionAnswer;
+
+        for (int i = 0; i < numberOfChoices; i++)
         {
-            dogUnit.gameObject.SetActive(false);
-            dogUnit.color = Color.white; // Reset to default color in case they were red before
+            if (i != correctAnswerIndex)
+            {
+                int potentialAnswer;
+                do
+                {
+                    int operation = Random.Range(0, 2) == 0 ? 1 : -1;
+                    potentialAnswer = questionAnswer + operation * Random.Range(1, 3);
+                } while (System.Array.IndexOf(answerOptions, potentialAnswer) != -1);
+
+                answerOptions[i] = potentialAnswer;
+            }
         }
 
-        // Activate the normal dog units for operand1
-        for (int i = 0; i < operand1 && i < dogUnits.Length; i++)
-        {
-            dogUnits[i].gameObject.SetActive(true);
-        }
+        ShuffleArray(answerOptions);
 
-        // Calculate the starting index for red dog units
-        // Since we are making dogs red from the end, we subtract operand2 from operand1
-        int startIndexForRedDogs = operand1 - operand2;
-
-        // Activate and color the red dog units for operand2 from the end
-        for (int i = startIndexForRedDogs; i < operand1 && i < dogUnits.Length; i++)
-        {
-            dogUnits[i].color = Color.red; // Change the color of the dog image to red
-        }
+        return answerOptions;
     }
-
 
     IEnumerator CheckAnswerWithDelay(int chosenAnswer)
     {
@@ -113,7 +102,9 @@ public class SubtractionQuiz : MonoBehaviour
             correctAnswersCount++;
         }
 
-        yield return new WaitForSeconds(1f);
+        Debug.Log($"Selected Answer: {chosenAnswer}. {feedbackText.text}");
+
+        yield return new WaitForSeconds(2f);
 
         feedbackText.text = "";
 
@@ -136,84 +127,17 @@ public class SubtractionQuiz : MonoBehaviour
         PlayerPrefs.SetFloat("Accuracy", ((float)correctAnswersCount / totalQuestions) * 100);
         PlayerPrefs.SetFloat("Rate", (totalQuestions/totalTime) * 60F);
         PlayerPrefs.SetInt("Wrong", totalQuestions - correctAnswersCount);
-        // Deactivate quiz UI
-        questionText.gameObject.SetActive(false);
-        foreach (var button in answerButtons)
-        {
-            button.gameObject.SetActive(false);
-        }
-        feedbackText.gameObject.SetActive(false);
-        questionNumberText.gameObject.SetActive(false);
-
-        accuracy = ((float)correctAnswersCount / totalQuestions) * 100;
-        totalWrongAnswers = totalQuestions - correctAnswersCount;
-
-        endTime = Time.time;
-        totalTime = endTime - startTime;
-
-        rate = (totalQuestions / totalTime) * 60f;
-
-        string currentDirectory = Application.persistentDataPath;
-        string filePath = Path.Combine(currentDirectory, "output.txt");
-
-        string csvContent = $"{totalQuestions},{correctAnswersCount},{accuracy},{rate:F2}";
-
-        try
-        {
-            File.WriteAllText(filePath, csvContent);
-        }
-        catch (Exception e)
-        {
-            Debug.LogError($"Error writing to file: {e.Message}");
-        }
-
-        scoreText.text = $"Quiz Completed!!\nScore: {correctAnswersCount}\nAccuracy: {accuracy}%\nRate: {rate:F2}/min\nWrong: {totalWrongAnswers}\n";
-
-        if (currentQuestionNumber >= totalQuestions)
-        {
-            StartCoroutine(LoadNextScene());
-        }
-    }
-
-    IEnumerator LoadNextScene()
-    {
-        yield return new WaitForSeconds(2f); // Adjust the delay as needed
-        SceneManager.LoadScene("ScoreBoard");
     }
 
     void ShuffleArray<T>(T[] array)
     {
         for (int i = array.Length - 1; i > 0; i--)
         {
-            int j = UnityEngine.Random.Range(0, i + 1);
+            int j = Random.Range(0, i + 1);
             T temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
     }
    
-}
-
-    int[] GenerateAnswerOptions()
-    {
-        int[] options = new int[numberOfChoices];
-
-        int correctAnswerIndex = UnityEngine.Random.Range(0, numberOfChoices);
-        options[correctAnswerIndex] = questionAnswer;
-
-        for (int i = 0; i < numberOfChoices; i++)
-        {
-            if (i != correctAnswerIndex)
-            {
-                int randomAnswer = UnityEngine.Random.Range(1, 10);
-                while (randomAnswer == questionAnswer || Array.IndexOf(options, randomAnswer) > -1)
-                {
-                    randomAnswer = UnityEngine.Random.Range(1, 10);
-                }
-                options[i] = randomAnswer;
-            }
-        }
-
-        return options;
-    }
 }
